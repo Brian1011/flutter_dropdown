e1b2ad42ccd79c4cfa6a74c09e8902aec5c8c92e
Brian Mutinda <mutindabrian26@gmail.com>
2022-05-09 10:31:00 +0300

    dropdown screen

---
+++ lib/screens/dropdown_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DropdownScreen extends StatefulWidget {
+  const DropdownScreen({Key key}) : super(key: key);
+
+  @override
+  _DropdownScreenState createState() => _DropdownScreenState();
+}
+
+class _DropdownScreenState extends State<DropdownScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
